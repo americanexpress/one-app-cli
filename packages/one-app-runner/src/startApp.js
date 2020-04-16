b6a7dbd76d2be1976bdd1201e82db7b81b4f6da6
James Singleton <jamesrsingleton1@gmail.com>
2020-04-15 21:57:09 -0700

    chore(eslint): fix eslint error

--- packages/one-app-runner/src/startApp.js
+++ packages/one-app-runner/src/startApp.js
@@ -77,9 +77,7 @@ module.exports = async function startApp({
     return command;
   };
 
-  const generateModuleMap = () => {
-    return moduleMapUrl ? `--module-map-url=${moduleMapUrl}` : '';
-  };
+  const generateModuleMap = () => (moduleMapUrl ? `--module-map-url=${moduleMapUrl}` : '');
 
   const command = `docker pull ${appDockerImage} && docker run -t -p 3000-3005:3000-3005 -e NODE_ENV=development ${generateEnvironmentVariableArgs(envVars)} ${generateModuleMountsArgs(modulesToServe)} ${appDockerImage} /bin/sh -c "${generateServeModuleCommands(modulesToServe)} ${generateSetMiddlewareCommand(parrotMiddlewareFile)} ${generateSetDevEndpointsCommand(devEndpointsFile)} node lib/server/index.js --root-module-name=${rootModuleName} ${generateModuleMap()} ${generateUseMocksFlag(parrotMiddlewareFile)}"`;
   const dockerProcess = spawn(command, { shell: true });
