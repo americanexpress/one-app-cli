4e5a371d0f0330ff4124848e663981be7976b8b1
Mike Tobia <Francois-Esquire@users.noreply.github.com>
2021-01-25 10:05:38 -0500

    fix(jest): update coverage
    Co-authored-by: James Singleton <james.singleton1@aexp.com>

--- jest.config.js
+++ jest.config.js
@@ -26,10 +26,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      statements: 99.31,
-      branches: 99.15,
-      lines: 99.71,
-      functions: 97,
+      statements: 99.38,
+      branches: 99.53,
+      functions: 97.27,
+      lines: 99.78,
     },
   },
   testPathIgnorePatterns: [
