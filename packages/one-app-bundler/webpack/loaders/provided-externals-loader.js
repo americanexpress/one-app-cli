58f4686145f4beec220e0f3813f3f087c95a6a2c
infoxicator <ruben@infoxication.net>
2021-02-15 17:51:22 +0000

    fix(externals): add getTenantRootModule validation

--- packages/one-app-bundler/webpack/loaders/provided-externals-loader.js
+++ packages/one-app-bundler/webpack/loaders/provided-externals-loader.js
@@ -31,7 +31,9 @@ ${match[1]}.appConfig = Object.assign({}, ${match[1]}.appConfig, {
   },
 });
 
+if(global.getTenantRootModule === undefined || (global.getTenantRootModule && global.getTenantRootModule().holocron.name === '${options.moduleName}')){
 global.getTenantRootModule = () => ${match[1]};
+}
 `;
   }
 
