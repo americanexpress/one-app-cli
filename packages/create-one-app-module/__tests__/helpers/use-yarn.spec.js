31ee101a3a8e80cf209849c50df0168698aad910
James Singleton <james.singleton1@aexp.com>
2021-03-09 12:57:31 -0800

    test(unit): add use-yarn test

---
+++ packages/create-one-app-module/__tests__/helpers/use-yarn.spec.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+const { execSync } = require('child_process');
+const shouldUseYarn = require('../../helpers/use-yarn');
+
+jest.mock('child_process', () => ({
+  execSync: jest.fn().mockResolvedValue(),
+}));
+
+describe('shouldUseYarn', () => {
+  it('it should return false to use yarn if "user-agent" starts with "npm"', () => {
+    process.env.npm_config_user_agent = 'npm/6.14.10 node/v12.20.1 darwin x64';
+    const useYarn = shouldUseYarn();
+    expect(useYarn).toBe(false);
+  });
+  it('should return true to use yarn if "user-agent" starts with "yarn"', () => {
+    process.env.npm_config_user_agent = 'yarn/1.22.10 npm/? node/v12.20.1 darwin x64';
+    const useYarn = shouldUseYarn();
+    expect(useYarn).toBe(true);
+  });
+  it('should execute execSync if no "user-agent" is set', () => {
+    process.env.npm_config_user_agent = '';
+    shouldUseYarn();
+
+    expect(execSync).toHaveBeenCalledTimes(1);
+  });
+});
