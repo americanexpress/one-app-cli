1b8a462bb6025076ea01ed80964508672c3bbe42
James Singleton <james.singleton1@aexp.com>
2021-03-17 16:03:48 -0700

    test(unit): add more useYarn tests

--- packages/create-one-app-module/__tests__/helpers/useYarn.spec.js
+++ packages/create-one-app-module/__tests__/helpers/useYarn.spec.js
@@ -17,17 +17,21 @@
 const { execSync } = require('child_process');
 const shouldUseYarn = require('../../helpers/useYarn');
 
-jest.mock('child_process', () => ({
-  execSync: jest.fn().mockResolvedValue(),
-}));
+jest.mock('child_process');
 
 describe('shouldUseYarn', () => {
   it('it should return false to use yarn if "user-agent" starts with "npm"', () => {
+    jest.mock('child_process', () => ({
+      execSync: jest.fn(),
+    }));
     process.env.npm_config_user_agent = 'npm/6.14.10 node/v12.20.1 darwin x64';
     const useYarn = shouldUseYarn();
     expect(useYarn).toBe(false);
   });
   it('should return true to use yarn if "user-agent" starts with "yarn"', () => {
+    jest.mock('child_process', () => ({
+      execSync: jest.fn(),
+    }));
     process.env.npm_config_user_agent = 'yarn/1.22.10 npm/? node/v12.20.1 darwin x64';
     const useYarn = shouldUseYarn();
     expect(useYarn).toBe(true);
@@ -37,5 +41,13 @@ describe('shouldUseYarn', () => {
     shouldUseYarn();
 
     expect(execSync).toHaveBeenCalledTimes(1);
+  });
+  it('should do something', () => {
+    process.env.npm_config_user_agent = '';
+    execSync.mockImplementation(() => {
+      throw new Error('Error');
+    });
+    const useYarn = shouldUseYarn();
+    expect(useYarn).toBe(false);
   });
 });
