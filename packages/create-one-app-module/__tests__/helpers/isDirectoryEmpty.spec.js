2305a1ca4cd918b2a253105704f9bf567a3f5f6a
James Singleton <james.singleton1@aexp.com>
2021-03-18 09:03:31 -0700

    test(unit): add more unit tests

--- packages/create-one-app-module/__tests__/helpers/isDirectoryEmpty.spec.js
+++ packages/create-one-app-module/__tests__/helpers/isDirectoryEmpty.spec.js
@@ -18,18 +18,13 @@ const path = require('path');
 const isDirectoryEmpty = require('../../helpers/isDirectoryEmpty');
 
 describe('isDirectoryEmpty', () => {
-  it('should', () => {
+  it('should return directory is empty', () => {
     const root = path.resolve('./packages/create-one-app-module/__tests__/__testfixtures__/empty');
     const isDirEmpty = isDirectoryEmpty(root, 'empty');
     expect(isDirEmpty).toBe(true);
   });
-  it('should not', () => {
-    const root = path.resolve('./packages/create-one-app-module/__tests__/__testfixtures__/notEmpty');
-    const isDirEmpty = isDirectoryEmpty(root, 'notEmpty');
-    expect(isDirEmpty).toBe(false);
-  });
 
-  it('should not use conflicted', () => {
+  it('should return directory is not empty', () => {
     const root = path.resolve('./packages/create-one-app-module/__tests__/__testfixtures__/conflicted');
     const isDirEmpty = isDirectoryEmpty(root, 'conflicted');
     expect(isDirEmpty).toBe(false);
