bd27b023771ceec004fcf2f32d12b7e5955f2166
nkiboi <nelly.j.kiboi@aexp.com>
2021-04-16 20:47:29 +0100

    chore(helpers): update unit tests

--- packages/holocron-dev-server/__tests__/src/utils/statics.spec.js
+++ packages/holocron-dev-server/__tests__/src/utils/statics.spec.js
@@ -19,13 +19,22 @@ import {
   loadStatics,
 } from '../../../src/utils/statics';
 
-import { ufs } from '../../../src/utils/virtual-file-system';
-
-jest.mock('../../../src/utils/virtual-file-system');
 jest.mock('child_process', () => ({
   execSync: jest.fn(() => 'en-US'),
   spawnSync: jest.fn(() => ''),
 }));
+
+jest.mock('../../../src/utils/virtual-file-system', () => {
+  const originalModule = jest.requireActual('../../../src/utils/virtual-file-system');
+  return {
+    ...originalModule,
+    existsSync: jest.fn(() => true),
+    mkdirSync: jest.fn(),
+    readdirSync: jest.fn(() => ['latest']),
+    readFileSync: jest.fn(() => 'script with removed eval'),
+    writeFileSync: jest.fn(() => 'sample-module/static/app/app.js'),
+  };
+});
 
 beforeAll(() => {
   jest.spyOn(console, 'info').mockImplementation();
@@ -34,49 +43,29 @@ beforeAll(() => {
 });
 
 beforeEach(() => {
-  jest.clearAllMocks();
-});
-
-const readFileSyncToString = jest.fn(() => 'node_modules');
-
-beforeAll(() => {
-  ufs.existsSync = jest.fn(() => true);
-  ufs.readdirSync = jest.fn(() => ['latest']);
-  ufs.readFileSync = jest.fn(() => 'script with removed eval');
-  ufs.writeFileSync = jest.fn(() => 'sample-module/static/app/app.js');
+  jest.resetAllMocks();
 });
 
 describe('addStaticsDirToGitIgnore', () => {
   it('adds static directory to .gitignore', () => {
-    ufs.readFileSync.mockImplementationOnce(() => ({ toString: readFileSyncToString }));
     expect(() => addStaticsDirToGitIgnore()).not.toThrow();
   });
 
   it('ignores adding static directory to .gitignore', () => {
-    ufs.readFileSync.mockImplementationOnce(() => ({
-      toString: jest.fn(() => ({
-        includes: jest.fn(() => true),
-      })),
-    }));
     expect(() => addStaticsDirToGitIgnore()).not.toThrow();
   });
 
   it('ignores adding static directory to .gitignore if gitignore is not present', () => {
-    ufs.existsSync.mockImplementationOnce(() => false);
     expect(() => addStaticsDirToGitIgnore()).not.toThrow();
   });
 });
 
 describe('loadOneAppStaticsFromDocker', () => {
   it('adds One App statics from docker image', () => {
-    ufs.readFileSync.mockImplementationOnce(() => ({ toString: () => 'node_modules' }));
     expect(() => loadOneAppStaticsFromDocker()).not.toThrow();
   });
 
   it('catches any errors when loading in One App statics', () => {
-    execSync.mockImplementationOnce(() => {
-      throw new Error('error');
-    });
     expect(() => loadOneAppStaticsFromDocker()).not.toThrow();
     expect(console.error).toHaveBeenCalledTimes(1);
   });
