deac2cd3e4c49eeb39b42cbc48c252fdfd66915e
Harrison Lara <harrisonlara97@gmail.com>
2021-09-15 15:24:58 -0500

    test: remove config option mock thats not needed

--- packages/one-app-bundler/__tests__/webpack/module/webpack.client.spec.js
+++ packages/one-app-bundler/__tests__/webpack/module/webpack.client.spec.js
@@ -17,7 +17,7 @@ const { validateWebpackConfig } = require('../../../test-utils');
 const getConfigOptions = require('../../../utils/getConfigOptions');
 const configGenerator = require('../../../webpack/module/webpack.client');
 
-jest.mock('../../../utils/getConfigOptions', () => jest.fn(() => ({ purgecss: {}, disableDevelopmentLegacyBundle: false })));
+jest.mock('../../../utils/getConfigOptions', () => jest.fn(() => ({ purgecss: {} })));
 
 jest.spyOn(process, 'cwd').mockImplementation(() => __dirname.split('/__tests__')[0]);
 
