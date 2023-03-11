4c7ac92d9f875d94767cc0502903dd8961a73085
Nickolas Oliver <Nickolas.Oliver@aexp.com>
2023-03-10 17:05:22 -0700

    test(packages): track packages sizes and contents

---
+++ packages/one-app-bundler/__tests__/pack.spec.js
@@ -0,0 +1,36 @@
+import path from 'node:path';
+import { spawnSync } from 'node:child_process';
+
+function sizeForHumans(bytes) {
+  if (bytes < 1024) {
+    return `${bytes} B`;
+  }
+  if (bytes < (1024 * 1024)) {
+    return `${(bytes / 1024).toFixed(1)} KiB / ${(bytes / 1000).toFixed(2)} KB`;
+  }
+  return `${(bytes / 1024 / 1024).toFixed(1)} MiB / ${(bytes / 1000 / 1000).toFixed(2)} MB`;
+}
+
+describe('publish artifact', () => {
+  let buildReport;
+
+  beforeAll(async () => {
+    const execution = spawnSync('npm', ['pack', '--json'], { cwd: path.resolve(__dirname) });
+    if (execution.error) {
+      throw execution.error;
+    }
+    buildReport = JSON.parse(execution.stdout);
+  });
+
+  it('is a known size', () => {
+    expect(buildReport).toHaveProperty('0.size', expect.any(Number));
+    expect(buildReport).toHaveProperty('0.unpackedSize', expect.any(Number));
+    expect(sizeForHumans(buildReport[0].size)).toMatchInlineSnapshot();
+    expect(sizeForHumans(buildReport[0].unpackedSize)).toMatchInlineSnapshot();
+  });
+
+  it('includes expected files', () => {
+    expect(buildReport).toHaveProperty('0.files', expect.any(Array));
+    expect(buildReport[0].files.map(({ path: filePath }) => filePath)).toMatchInlineSnapshot();
+  });
+});
