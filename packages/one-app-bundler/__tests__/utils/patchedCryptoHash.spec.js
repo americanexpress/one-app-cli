efa451a3f3417db9eb54c197fef72293ddbce63e
bbist1 <bishnu.bista1@aexp.com>
2023-04-13 15:34:11 -0700

    fix(cyptoHash): added test case

--- packages/one-app-bundler/__tests__/utils/patchedCryptoHash.spec.js
+++ packages/one-app-bundler/__tests__/utils/patchedCryptoHash.spec.js
@@ -13,7 +13,17 @@
  */
 const crypto = require('crypto');
 
-// Monkey Patch for unsupported hash algo. Needed to support Node >=17.
-// https://github.com/webpack/webpack/issues/13572#issuecomment-923736472
-const originalCreateHash = crypto.createHash;
-crypto.createHash = (algo) => originalCreateHash(algo === 'md4' ? 'sha256' : algo);
+const createHashSpy = jest.spyOn(crypto, 'createHash');
+
+describe('patchedCryptoHash', () => {
+  it('should replace md4 with sha256 as default hash algo', () => {
+    require('../../utils/patchedCryptoHash');
+    crypto.createHash('md4');
+    expect(createHashSpy).toHaveBeenCalledWith('sha256');
+  });
+  it('should keep hash if different than md4', () => {
+    require('../../utils/patchedCryptoHash');
+    crypto.createHash('sha512');
+    expect(createHashSpy).toHaveBeenCalledWith('sha512');
+  });
+});
